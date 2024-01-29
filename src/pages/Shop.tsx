07fee310ab93a84e15cc500d932edcb65753dbc0
nalosara <sara.nalo@stu.ibu.edu.ba>
2024-01-29 17:27:14 +0100

    shop edit

--- src/pages/Shop.tsx
+++ src/pages/Shop.tsx
@@ -1,9 +1,12 @@
 import ProductList from "../components/ProductList";
+import { BASE_URL } from "../constants";
 
 type ShopProps = {};
 
 const Shop = ({}: ShopProps) => {
+  console.log("base url", BASE_URL);
   return (
+    
     <div style={{ marginTop: 150, marginBottom: 20 }}>
       <ProductList />
     </div>
