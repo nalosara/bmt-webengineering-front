937dfd401efc8844a22a403663231afd4786b2dc
nalosara <sara.nalo@stu.ibu.edu.ba>
2024-01-29 18:06:48 +0100

    websocket url edit

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -26,7 +26,7 @@ const ProductList = ({}: ProductListProps) => {
   let decodedToken: CustomJwtPayload;
   let authorities: string[] | undefined;
 
-  const socketUrl = `ws://bmtwebshop-3a305a901ea3.herokuapp.com/websocket?token=${userToken}`;
+  const socketUrl = `wss://bmtwebshop-3a305a901ea3.herokuapp.com/websocket?token=${userToken}`;
   const { sendJsonMessage } = useWebSocket(socketUrl);
   
   useEffect(() => {
